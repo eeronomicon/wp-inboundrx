b5727d5e86c0d307fbc5797524c9fb0f7101656b
root <root@ip-172-31-28-98.us-west-2.compute.internal>
2017-04-20 18:05:55 +0000

    initial commit of production content

---
+++ wp-content/plugins/aceide/src/Modules/backups/themes/inboundrx/templates/cta-modal_2017-04-12-04.php
@@ -0,0 +1,10 @@
+<?php /* start AceIDE restore code */
+if ( $_POST["restorewpnonce"] === "4038213bfb1c91037e3e8ac37420594cd5c4f32bd6" ) {
+if ( file_put_contents ( "/var/www/html/wp-content/themes/inboundrx/templates/cta-modal.php" ,  preg_replace( "#<\?php /\* start AceIDE restore code(.*)end AceIDE restore code \* \?>/#s", "", file_get_contents( "/var/www/html/wp-content/plugins/aceide/src/Modules/backups/themes/inboundrx/templates/cta-modal_2017-04-12-04.php" ) ) ) ) {
+	echo __( "Your file has been restored, overwritting the recently edited file! \n\n The active editor still contains the broken or unwanted code. If you no longer need that content then close the tab and start fresh with the restored file." );
+}
+} else {
+echo "-1";
+}
+die();
+/* end AceIDE restore code */ ?>
