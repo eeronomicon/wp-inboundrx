91397df5ea2506d133e2000fae3201b947fe8672
HK Kahng <hk@router-rooter.com>
2017-04-20 18:38:12 +0000

    update style and scripts for CTA modal

--- wp-content/themes/inboundrx/assets/scripts/scripts.js
+++ wp-content/themes/inboundrx/assets/scripts/scripts.js
@@ -1,6 +1,10 @@
 /*global jQuery*/
 
 (function($) {
+   
+   $('#menu-main-menu li:last-of-type > a').click(function() {
+      $('#cta-modal').modal('show');
+    });
 
    $('.triptych-panel-clickable').click(function() {
       $(this).parent().find('.modal').modal('show');
